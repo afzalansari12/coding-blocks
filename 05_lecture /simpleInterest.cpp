3a4f8bd98566b51a8dbe48cf221b8772bc71e10b
afzalansari12 <afzalansari12ab@gmail.com>
2026-03-22 14:52:23 +0530

    added simple interest program

---
+++ 05_lecture /simpleInterest.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int p, r,t;
+    cout << "enter p, r, t";
+    cin >> p >> r >> t;
+    int si = (p*r*t)/100.0;
+    cout <<  " simple interest = " << si << endl;
+
+return 0;
+}
