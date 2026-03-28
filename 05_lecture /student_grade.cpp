e635e6924641beaaaa80c498d4b516450dd33570
afzalansari12 <afzalansari12ab@gmail.com>
2026-03-28 22:25:37 +0530

    commit

---
+++ 05_lecture /student_grade.cpp
@@ -0,0 +1,64 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int marks;
+//     cin >> marks;
+
+//     if(marks >= 0 && marks <= 100){
+
+//     if(marks >= 90){
+//         cout << "grade A"<< endl;
+//     }else if(marks < 90 && marks > 80){
+//         cout << " grade B" << endl;
+//     }else if(marks <= 80 && marks > 70){
+//         cout << " grade c" << endl;
+//     }else if(marks <= 70 && marks > 60){
+//         cout << " grade D" << endl;
+//     }else if(marks <= 60 && marks > 50){
+//         cout << " grade E" << endl;
+//     }else{
+//         cout << " fails";
+//      }
+
+//     }else{
+//         cout << "invalid marks" << endl ;
+//     }
+// return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char grade;
+//     cin >> grade;
+//     switch (grade)
+//     {
+   
+//     case 'A': cout << "v.good" <<endl;break;
+//     case 'B': cout << "good" <<endl;break;
+//     case 'C': cout << "poor" <<endl;break;
+//     case 'E': cout << "v.poor" <<endl;break;
+//     case 'F': cout << "fails" <<endl;break;
+//     default: cout << "invalid " << endl; break;  
+//     }
+// return 0;
+// }
+
+//  wAP to read a 5 number by while loop
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+//     int n = 5;
+//     int i = 1;
+
+//    while(n  > 0){
+
+//       cout << i << endl;
+
+//       i++;
+//       n--;    
+//     }
+// return 0;
+// }
