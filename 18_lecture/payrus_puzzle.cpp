b5c102cdac21c9b3438178051c6503bf81d860f9
afzalansari12 <afzalansari12ab@gmail.com>
2026-07-04 16:36:43 +0530

    cf ques

---
+++ 18_lecture/payrus_puzzle.cpp
@@ -0,0 +1,69 @@
+// #include<iostream>
+// #include<algorithm>
+
+// using namespace std;
+// int main(){
+//     int t;
+//     cin >> t;
+//      int ans = 0;
+
+
+//     while(t--){
+
+//     int n;
+//     cin >> n;
+//     int c;
+//     cin >> c;
+
+
+//     int arr[n];
+//     int brr[n];
+
+//     for(int i = 0; i < n; i++){
+//         cin >> arr[i];
+//     }
+
+
+//      for(int i = 0; i < n; i++){
+//         cin >> brr[i];
+//     }
+
+//      sort(arr,arr+n);
+//      sort(brr,brr+n);
+      
+    
+
+//     for(int i = 0; i < n; i++){
+//            if(arr[i] < brr[i]){
+//             ans = -1;
+//             break;
+//            } else if(arr[i] > brr[i]){
+//                while(arr[i]!= brr[i]){
+//                  arr[i]--;
+//                  ans++;
+//                }
+               
+//            }else{
+//                if(i==n-1){
+//                 ans = c;
+//                }
+//            }
+//     }
+
+//     cout << ans << '\n';
+ 
+//     }
+// return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        
+    }
+return 0;
+}
